cf3fe1f0740920961c20a7b6df6181d5e39ac71f
Sakar <sakarbaral10@outlook.com>
2021-02-21 12:57:05 +0545

    Changes to make it work in heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.get('/main',function (req,res) {
+app.get('/',function (req,res) {
   res.render('main');
   });
 
